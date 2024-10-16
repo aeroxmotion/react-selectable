d55bc388900f1761383b6b8c9e9f20e0046ce297
Camilo Rodriguez <aeroxmotion@hotmail.com>
2024-10-15 20:12:26 -0500

    feat!: allow custom styling on selection box by using CSS Custom Properties

--- src/components/SelectionBox.tsx
+++ src/components/SelectionBox.tsx
@@ -2,32 +2,47 @@ import React, { memo } from 'react'
 
 import { useSelectionBox } from '../hooks/useSelectionBox'
 
-export interface SelectionBoxProps
-  extends React.HTMLAttributes<HTMLDivElement> {
-  // Leave empty for now
+export interface SelectionBoxProps extends React.HTMLAttributes<HTMLElement> {
+  /**
+   * Custom tag (defaults to `div`)
+   */
+  tag?: keyof HTMLElementTagNameMap
+
+  /**
+   * Prefix for CSS custom properties to style your selection box.
+   * Defaults to `selection-box`.
+   *
+   * __Note:__ Instead of styling the selection box's element directly,
+   * we just use custom properties to hold the current state
+   * of the selection box, so you can style it as you want.
+   */
+  customPropertyPrefix?: string
 }
 
 const BaseSelectionBox: React.FC<SelectionBoxProps> = ({
+  tag = 'div',
   className = 'selection-box',
+  customPropertyPrefix = 'selection-box',
   ...attributes
 }) => {
   const selectionBox = useSelectionBox()
 
-  return (
-    selectionBox && (
-      <div
-        {...attributes}
-        className={className}
-        style={{
-          top: selectionBox.y,
-          left: selectionBox.x,
-          width: selectionBox.width,
-          height: selectionBox.height,
-          ...attributes.style,
-        }}
-      />
-    )
-  )
+  // If no selection box, just remove it from the DOM
+  if (!selectionBox) {
+    return null
+  }
+
+  return React.createElement(tag, {
+    ...attributes,
+    className,
+    style: {
+      [`--${customPropertyPrefix}-x`]: `${selectionBox.x}px`,
+      [`--${customPropertyPrefix}-y`]: `${selectionBox.y}px`,
+      [`--${customPropertyPrefix}-width`]: `${selectionBox.width}px`,
+      [`--${customPropertyPrefix}-height`]: `${selectionBox.height}px`,
+      ...attributes.style,
+    },
+  })
 }
 
 export const SelectionBox = memo(BaseSelectionBox)
